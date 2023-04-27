1be8932c8e16043dca39a738d43ba3c9483a2b7d
h sabri <hsabri75@gmail.com>
2023-04-27 17:42:02 +0300

    cypress screenshots are added

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -2,15 +2,16 @@
 
 it('Open page',()=>{
     cy.visit('http://127.0.0.1:5500/dist/index.html')
-
+    cy.screenshot("openscreen")
     cy.get('#playbutton').should('exist')
     cy.contains('#guesstext').should('not.exist')
     cy.contains('#guessbutton').should('not.exist')
     cy.get('#guesslist').should('exist')
     cy.get('#guesslist').children().should('have.length', 0)
+
 })
 
-it('Make Guess',()=>{
+it('New Game',()=>{
     cy.visit('http://127.0.0.1:5500/dist/index.html')
 
     cy.get('#playbutton').click()    
@@ -18,33 +19,60 @@ it('Make Guess',()=>{
     cy.get('#guesstext').should('exist')
     cy.get('#guessbutton').should('exist')
     cy.get('#guesslist').children().should('have.length', 0)
+    cy.wait(1000)
+    cy.screenshot("newgame")
+})
+it('Make Guess 123',()=>{
+    cy.visit('http://127.0.0.1:5500/dist/index.html')
+
+    cy.get('#playbutton').click()  
 
     cy.get('#guesstext').type('123')
     cy.get('#guessbutton').click()
     cy.get('#warningtext').contains('Guess should have 4 digits')
+    cy.wait(1000)
+    cy.screenshot("guess_123")
+})
+it('Make Guess 1233',()=>{
+    cy.visit('http://127.0.0.1:5500/dist/index.html')
 
-    cy.get('#guesstext').clear()
+    cy.get('#playbutton').click() 
     cy.get('#guesstext').type('1233')
     cy.get('#guessbutton').click()
     cy.get('#warningtext').contains('Repeating digits not allowed')
+    cy.wait(1000)
+    cy.screenshot("guess_1233")
+})
+it('Make Guess 1234',()=>{
+    cy.visit('http://127.0.0.1:5500/dist/index.html')
 
-    cy.get('#guesstext').clear()
+    cy.get('#playbutton').click()
     cy.get('#guesstext').type('1234')
     cy.get('#guessbutton').click()
     cy.get('#guesslist').children().should('have.length', 1) 
-    
+    cy.wait(1000)
+    cy.screenshot("guess_1234")
+})
+it('Restart',()=>{
+    cy.visit('http://127.0.0.1:5500/dist/index.html')
+
+    cy.get('#playbutton').click()
+    cy.get('#guesstext').type('1234')
+    cy.get('#guessbutton').click()
     cy.get('#playbutton').click()
 
     cy.get('#guesstext').should('exist')
     cy.get('#guessbutton').should('exist')
-    cy.get('#guesslist').children().should('have.length', 0)  
+    cy.get('#guesslist').children().should('have.length', 0) 
+    cy.wait(1000)
+    cy.screenshot("restart") 
 
 })
 
 it('Success',()=>{
     cy.visit('http://127.0.0.1:5500/dist/index.html')
-    cy.get('#playbutton').click()
 
+    cy.get('#playbutton').click()
     cy.get('#guesstext').type('1234')
     cy.get('#guessbutton').click()    
 
@@ -52,5 +80,7 @@ it('Success',()=>{
         cy.get('#guesstext').clear()
         cy.get('#guesstext').type($value.text())
         cy.get('#guessbutton').click()
+        cy.wait(1000)
+        cy.screenshot("success") 
     })
 })
